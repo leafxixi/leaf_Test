fc3437c11e001f40b0bdeb7b0481c78a147ce5a4
leafxixi <leaf.xixi@gmail.com>
2014-08-18 11:41:45 +0800

    Leaf.xixi Test 1

---
+++ allTestModules.js
@@ -0,0 +1,53 @@
+define([
+  'doh/main', 'require'
+], function(doh, require) {
+  // var url = '/arcgis/apps/webappbuilder/stemapp/jimu.js/tests/my-test-loader.html';
+  // doh.register('widget manager tests',url + "?file=test-widgetmanager",30000);
+  // //doh.register('app config tests',url + "?file=test-config",30000);
+  // doh.register('data manager tests',url + "?file=test-datamanager",30000);
+  // doh.register('utils tests',url + "?file=test-utils",30000);
+  // // doh.register('jquery loader tests',url + "?file=test-jquery-loader",30000);
+  // doh.register('order loader tests',url + "?file=test-order-loader",30000);
+
+  if (doh.isBrowser) {
+    //doh.register(" tests.Overview", require.toUrl("./testwidgets/Overview/test_Overview.html"), 3000000);
+    //doh.register(" tests.AttributeTable", require.toUrl("./testwidgets/AttributeTable/testA_AttributeTable.html"), 3000000);
+	//doh.register(" tests.AttributeTable", require.toUrl("./testwidgets/AttributeTable/testB_AttributeTable.html"), 3000000);	
+	//doh.register(" tests.Basemap Gallery", require.toUrl("./testwidgets/Basemap Gallery/testA_Basemap Gallery.html"), 3000000);
+	//doh.register(" tests.Basemap Gallery", require.toUrl("./testwidgets/Basemap Gallery/testB_Basemap Gallery.html"), 3000000);
+	//doh.register(" tests.Bookmark", require.toUrl("./testwidgets/Bookmark/testA_Bookmark.html"), 3000000);
+	//doh.register(" tests.Bookmark", require.toUrl("./testwidgets/Bookmark/testB_Bookmark.html"), 3000000);
+	//doh.register(" tests.Coordinate", require.toUrl("./testwidgets/Coordinate/testA_Coordinate.html"), 3000000);
+	//doh.register(" tests.Coordinate", require.toUrl("./testwidgets/Coordinate/testB_Coordinate.html"), 3000000);
+	//doh.register(" tests.Chart", require.toUrl("./testwidgets/Chart/testA_Chart.html"), 3000000);
+	//doh.register(" tests.Chart", require.toUrl("./testwidgets/Chart/testB_Chart.html"), 3000000);
+	//doh.register(" tests.Directions", require.toUrl("./testwidgets/Directions/testA_Directions.html"), 3000000);
+	//doh.register(" tests.Directions", require.toUrl("./testwidgets/Directions/testB_Directions.html"), 3000000);
+	//doh.register(" tests.Draw", require.toUrl("./testwidgets/Draw/testA_Draw.html"), 3000000);
+	//doh.register(" tests.Draw", require.toUrl("./testwidgets/Draw/testB_Draw.html"), 3000000);
+	//doh.register(" tests.Edit", require.toUrl("./testwidgets/Edit/testA_Edit.html"), 3000000);
+	//doh.register(" tests.Edit", require.toUrl("./testwidgets/Edit/testB_Edit.html"), 3000000);
+	//doh.register(" tests.Geoprocessing", require.toUrl("./testwidgets/Geoprocessing/testA_Geoprocessing.html"), 3000000);
+	//doh.register(" tests.Geoprocessing", require.toUrl("./testwidgets/Geoprocessing/testB_Geoprocessing.html"), 3000000);
+	//doh.register(" tests.Header Controller", require.toUrl("./testwidgets/Header Controller/testA_Header Controller.html"), 3000000);
+	//doh.register(" tests.Header Controller", require.toUrl("./testwidgets/Header Controller/testB_Header Controller.html"), 3000000);
+	//doh.register(" tests.HomeButton", require.toUrl("./testwidgets/HomeButton/testA_HomeButton.html"), 3000000);
+	//doh.register(" tests.HomeButton", require.toUrl("./testwidgets/HomeButton/testB_HomeButton.html"), 3000000);
+	//doh.register(" tests.LayerList", require.toUrl("./testwidgets/LayerList/testA_LayerList.html"), 3000000);
+	//doh.register(" tests.LayerList", require.toUrl("./testwidgets/LayerList/testB_LayerList.html"), 3000000);
+	//doh.register(" tests.Legend", require.toUrl("./testwidgets/Legend/testA_Legend.html"), 3000000);
+	//doh.register(" tests.Legend", require.toUrl("./testwidgets/Legend/testB_Legend.html"), 3000000);
+	//doh.register(" tests.Measuerment", require.toUrl("./testwidgets/Measuerment/testA_Measuerment.html"), 3000000);
+	//doh.register(" tests.Measuerment", require.toUrl("./testwidgets/Measuerment/testB_Measuerment.html"), 3000000);
+	//doh.register(" tests.My Location", require.toUrl("./testwidgets/My Location/testA_My Location.html"), 3000000);
+	//doh.register(" tests.My Location", require.toUrl("./testwidgets/My Location/testB_My Location.html"), 3000000);
+	//doh.register(" tests.Print", require.toUrl("./testwidgets/Print/testA_Print.html"), 3000000);
+	//doh.register(" tests.Print", require.toUrl("./testwidgets/Print/testB_Print.html"), 3000000);
+	//doh.register(" tests.Query", require.toUrl("./testwidgets/Query/testA_Query.html"), 3000000);
+	//doh.register(" tests.Query", require.toUrl("./testwidgets/Query/testB_Query.html"), 3000000);
+	//doh.register(" tests.Scalebar", require.toUrl("./testwidgets/Scalebar/testA_Scalebar.html"), 3000000);
+	//doh.register(" tests.Scalebar", require.toUrl("./testwidgets/Scalebar/testB_Scalebar.html"), 3000000);
+	
+	doh.register(" tests.Framework", require.toUrl("./testwidgets/Framework/testB_Framework.html"), 3000000);
+  }
+});
